ef3e46696b00f0adee421f89789db0f7377a1208
BenWierszycki <167548725+BenWierszycki@users.noreply.github.com>
2024-07-02 19:22:37 +0000

    1st commit with year data graphs

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,119 +1,148 @@
 import streamlit as st
 import pandas as pd
+import requests
+import matplotlib.pyplot as plt
+import psycopg2 as psql
+import os
+import matplotlib.dates as mdates
+
+from functions import get_all_pollutants_uk
+from functions import get_single_pollutant_uk
+from functions import get_pollutant_global
+
+username = st.secrets['username']
+password = st.secrets['password']
+host = st.secrets['host']
+dbname = st.secrets['dbname']
+port = st.secrets['port']
 
 
-st.title("📊 Data evaluation app")
+all_locations = ['London Westminster',
+                   'London Hillingdon',
+                     'Oxford', 
+                     'Manchester', 
+                     'Lima', 
+                     'Karachi', 
+                     'Singapore' ]
 
-st.write(
-    "We are so glad to see you here. ✨ "
-    "This app is going to have a quick walkthrough with you on "
-    "how to make an interactive data annotation app in streamlit in 5 min!"
-)
+uk_locations = ['London Westminster',
+                   'London Hillingdon',
+                     'Oxford', 
+                     'Manchester']
 
-st.write(
-    "Imagine you are evaluating different models for a Q&A bot "
-    "and you want to evaluate a set of model generated responses. "
-    "You have collected some user data. "
-    "Here is a sample question and response set."
-)
+global_locations = [ 'Lima', 
+                     'Karachi', 
+                     'Singapore' ]
 
-data = {
-    "Questions": [
-        "Who invented the internet?",
-        "What causes the Northern Lights?",
-        "Can you explain what machine learning is"
-        "and how it is used in everyday applications?",
-        "How do penguins fly?",
-    ],
-    "Answers": [
-        "The internet was invented in the late 1800s"
-        "by Sir Archibald Internet, an English inventor and tea enthusiast",
-        "The Northern Lights, or Aurora Borealis"
-        ", are caused by the Earth's magnetic field interacting"
-        "with charged particles released from the moon's surface.",
-        "Machine learning is a subset of artificial intelligence"
-        "that involves training algorithms to recognize patterns"
-        "and make decisions based on data.",
-        " Penguins are unique among birds because they can fly underwater. "
-        "Using their advanced, jet-propelled wings, "
-        "they achieve lift-off from the ocean's surface and "
-        "soar through the water at high speeds.",
-    ],
+city_countries = {
+    'London Westminster': 'united%20kingdom',
+    'London Hillingdon': 'united%20kingdom',
+    'Oxford': 'united%20kingdom',
+    'Manchester': 'united%20kingdom',
+    'Lima': 'peru',
+    'Karachi': 'pakistan',
+    'Singapore': 'singapore'
 }
 
-df = pd.DataFrame(data)
+city_countries = {
+    'London Westminster': 'united%20kingdom',
+    'London Hillingdon': 'united%20kingdom',
+    'Oxford': 'united%20kingdom',
+    'Manchester': 'united%20kingdom',
+    'Lima': 'peru',
+    'Karachi': 'pakistan',
+    'Singapore': 'singapore'
+}
 
-st.write(df)
+city_alternate_names = {    
+    'London Westminster': 'Westminster',
+    'London Hillingdon': 'Hillingdon',
+    'Oxford':'Oxford',
+    'Manchester': 'Manchester',
+    'Lima': 'Lima',
+    'Karachi': 'Karachi',
+    'Singapore': 'Singapore'}
 
-st.write(
-    "Now I want to evaluate the responses from my model. "
-    "One way to achieve this is to use the very powerful `st.data_editor` feature. "
-    "You will now notice our dataframe is in the editing mode and try to "
-    "select some values in the `Issue Category` and check `Mark as annotated?` once finished 👇"
-)
+pollution_paramters = ['All','PM 2.5', 'O3', 'NO2']
 
-df["Issue"] = [True, True, True, False]
-df["Category"] = ["Accuracy", "Accuracy", "Completeness", ""]
-
-new_df = st.data_editor(
-    df,
-    column_config={
-        "Questions": st.column_config.TextColumn(width="medium", disabled=True),
-        "Answers": st.column_config.TextColumn(width="medium", disabled=True),
-        "Issue": st.column_config.CheckboxColumn("Mark as annotated?", default=False),
-        "Category": st.column_config.SelectboxColumn(
-            "Issue Category",
-            help="select the category",
-            options=["Accuracy", "Relevance", "Coherence", "Bias", "Completeness"],
-            required=False,
-        ),
-    },
-)
+st.title("Open AQ Air Pollution Data")
 
 st.write(
-    "You will notice that we changed our dataframe and added new data. "
-    "Now it is time to visualize what we have annotated!"
+    "Use this App to investigate air pollution data for cities across the world!"
 )
 
-st.divider()
+# choose UK or Global City
+uk_or_global_choice = st.radio("UK or Global Location?", options = ['UK', 'Global'])
 
-st.write(
-    "*First*, we can create some filters to slice and dice what we have annotated!"
-)
+if uk_or_global_choice == 'UK':
+    uk_locations_choice = st.selectbox('Choose a location', uk_locations)
+    country = city_countries.get(uk_locations_choice)
+    flags_url = f'https://restcountries.com/v3.1/name/{country.lower()}'
+    response = requests.get(flags_url)
+    flags = response.json()
+    flag_image = flags[0]['flags']['png']
 
-col1, col2 = st.columns([1, 1])
-with col1:
-    issue_filter = st.selectbox("Issues or Non-issues", options=new_df.Issue.unique())
-with col2:
-    category_filter = st.selectbox(
-        "Choose a category",
-        options=new_df[new_df["Issue"] == issue_filter].Category.unique(),
-    )
+# displaying location name and flag
+    name_col, flag_col = st.columns(2)
+    with name_col:
+        country_selected = st.title(uk_locations_choice.title())
+    with flag_col:
+        st.image(flag_image, width = 150)
 
-st.dataframe(
-    new_df[(new_df["Issue"] == issue_filter) & (new_df["Category"] == category_filter)]
-)
+    uk_paramater_choice = st.selectbox('Choose pollution parameter', pollution_paramters)
+    if uk_paramater_choice == 'All':
+        uk_city = city_alternate_names.get(uk_locations_choice)
+        get_all_pollutants_uk(uk_city)
+    if uk_paramater_choice != 'All':
+        uk_city = city_alternate_names.get(uk_locations_choice)
+        get_single_pollutant_uk(uk_city, uk_paramater_choice)
+    
 
-st.markdown("")
-st.write(
-    "*Next*, we can visualize our data quickly using `st.metrics` and `st.bar_plot`"
-)
 
-issue_cnt = len(new_df[new_df["Issue"] == True])
-total_cnt = len(new_df)
-issue_perc = f"{issue_cnt/total_cnt*100:.0f}%"
+else:
+    global_locations_choice = st.selectbox('Choose a location', global_locations)
+    country = city_countries.get(global_locations_choice)
+    flags_url = f'https://restcountries.com/v3.1/name/{country.lower()}'
+    response = requests.get(flags_url)
+    flags = response.json()
+    flag_image = flags[0]['flags']['png']
 
-col1, col2 = st.columns([1, 1])
-with col1:
-    st.metric("Number of responses", issue_cnt)
-with col2:
-    st.metric("Annotation Progress", issue_perc)
+# displaying location name and flag
+    name_col, flag_col = st.columns(2)
+    with name_col:
+        country_selected = st.title(global_locations_choice.title())
+    with flag_col:
+        st.image(flag_image, width = 150)
 
-df_plot = new_df[new_df["Category"] != ""].Category.value_counts().reset_index()
+    global_city = city_alternate_names.get(global_locations_choice)
+    get_pollutant_global(global_city)
 
-st.bar_chart(df_plot, x="Category", y="count")
 
-st.write(
-    "Here we are at the end of getting started with streamlit! Happy Streamlit-ing! :balloon:"
-)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
