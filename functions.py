e80532f933ded53f9561465590353b60f7df56a6
BenWierszycki <167548725+BenWierszycki@users.noreply.github.com>
2024-07-03 19:18:03 +0000

    updated functions

--- functions.py
+++ functions.py
@@ -5,6 +5,8 @@ import matplotlib.pyplot as plt
 import psycopg2 as psql
 import os
 import matplotlib.dates as mdates
+import seaborn
+from datetime import datetime, timedelta
 
 username = st.secrets['username']
 password = st.secrets['password']
@@ -12,191 +14,242 @@ host = st.secrets['host']
 dbname = st.secrets['dbname']
 port = st.secrets['port']
 
-def get_all_pollutants_uk(uk_city):
-            
-        conn = psql.connect(
-        dbname=dbname,
-        user=username,
-        password=password,
-        host=host,
-        port=port
-        )
 
-        cur = conn.cursor()
-
-        # SQL query to select data
-        sql_query = f"""
-            SELECT datetime, {uk_city}_pm25, {uk_city}_o3, {uk_city}_no2
-            FROM student.bw_air_pollution_data
-            WHERE {uk_city}_pm25 IS NOT NULL
-            AND {uk_city}_NO2 < 200
-            ORDER BY datetime
-        """
-
-        cur.execute(sql_query)
-        rows = cur.fetchall()
-
-        # Close cursor and connection
-        cur.close()
-        conn.close()
-
-        # Convert query result to a pandas DataFrame
-        df = pd.DataFrame(rows, columns=['datetime', f'{uk_city}_pm25', f'{uk_city}_o3', f'{uk_city}_no2'])
-
-        # Convert datetime column to datetime type
-        df['datetime'] = pd.to_datetime(df['datetime'])
-
-        # Setting the font size for labels
-        plt.rcParams.update({
-            'font.size': 14,
-            'axes.titlesize': 20,
-            'axes.labelsize': 18,
-            'xtick.labelsize': 14,
-            'ytick.labelsize': 14,
-            'legend.fontsize': 16
-        })
-        
-        # Plotting with matplotlib
-        plt.figure(figsize=(12, 6))
-        plt.plot(df['datetime'], df[f'{uk_city}_pm25'], label='PM2.5', linestyle='-', color='r', linewidth=0.75)
-        plt.plot(df['datetime'], df[f'{uk_city}_o3'], label='O3', linestyle='-', color='g', linewidth=0.75)
-        plt.plot(df['datetime'], df[f'{uk_city}_no2'], label='NO2', linestyle='-', color='b', linewidth=0.75)
-
-        # Format the date to MM-YYYY
-        plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m-%Y'))
-
-        plt.legend()
-        plt.xlabel('Date')
-        plt.ylabel('Pollution Level μg/m3')
-        plt.title(f'Pollution Data for {uk_city}')
-        plt.tight_layout()
-
-        # Display the plot in Streamlit
-        return st.pyplot(plt)
 
 ###########################################################################################
+# SINGLE POLLUTANT UK
 
-def get_single_pollutant_uk(uk_city, uk_paramater_choice):
-            
-        conn = psql.connect(
-        dbname=dbname,
-        user=username,
-        password=password,
-        host=host,
-        port=port
-        )
-
-        cur = conn.cursor()
-
-        # SQL query to select data
-        sql_query = f"""
-            SELECT datetime, {uk_city}_{uk_paramater_choice.lower()}
-            FROM student.bw_air_pollution_data
-            WHERE {uk_city}_{uk_paramater_choice.lower()} IS NOT NULL
-            AND {uk_city}_{uk_paramater_choice.lower()} < 200
-            ORDER BY datetime
-        """
-
-        cur.execute(sql_query)
-        rows = cur.fetchall()
-
-        # Close cursor and connection
-        cur.close()
-        conn.close()
-
-        # Convert query result to a pandas DataFrame
-        df = pd.DataFrame(rows, columns=['datetime', f'{uk_city}_{uk_paramater_choice.lower()}'])
-
-        # Convert datetime column to datetime type
-        df['datetime'] = pd.to_datetime(df['datetime'])
-
-        # Setting the font size for labels
-        plt.rcParams.update({
-            'font.size': 14,
-            'axes.titlesize': 20,
-            'axes.labelsize': 18,
-            'xtick.labelsize': 14,
-            'ytick.labelsize': 14,
-            'legend.fontsize': 16
-        })
-
-        # Plotting with matplotlib
-        plt.figure(figsize=(12, 6))
-        plt.plot(df['datetime'], df[f'{uk_city}_{uk_paramater_choice.lower()}'], label=f'{uk_paramater_choice}', linestyle='-', color='b', linewidth=0.75)
-
-        # Format the date to MM-YYYY
-        plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m-%Y'))
-
-        plt.legend()
-        plt.xlabel('Date')
-        plt.ylabel('Pollution Level μg/m3')
-        plt.title(f'{uk_paramater_choice.upper()} Pollution Data for {uk_city}')
-        plt.tight_layout()
-
-        # Display the plot in Streamlit
-        return st.pyplot(plt)
 
 ##############################################################################################
+# SINGLE POLLUTANT GLOBAL
 
-def get_pollutant_global(global_city):
+def get_pollutant_global(renamed_city):
             
-        conn = psql.connect(
+    conn = psql.connect(
         dbname=dbname,
         user=username,
         password=password,
         host=host,
         port=port
-        )
+    )
 
-        cur = conn.cursor()
+    cur = conn.cursor()
 
-        # SQL query to select data
-        sql_query = f"""
-            SELECT datetime, {global_city}_pm25
-            FROM student.bw_air_pollution_data
-            WHERE {global_city}_pm25 IS NOT NULL 
-            AND {global_city}_pm25 != -999
-            ORDER BY datetime
-        """
+    # SQL query to select data
+    sql_query = f"""
+        SELECT datetime, {renamed_city}_pm25
+        FROM student.bw_air_pollution_data
+        WHERE {renamed_city}_pm25 IS NOT NULL 
+        AND {renamed_city}_pm25 != -999
+        ORDER BY datetime
+    """
 
-        cur.execute(sql_query)
-        rows = cur.fetchall()
+    cur.execute(sql_query)
+    rows = cur.fetchall()
 
-        # Close cursor and connection
-        cur.close()
-        conn.close()
+    # Close cursor and connection
+    cur.close()
+    conn.close()
 
-        # Convert query result to a pandas DataFrame
-        df = pd.DataFrame(rows, columns=['datetime', f'{global_city}_pm25'])
+    # Convert query result to a pandas DataFrame
+    df = pd.DataFrame(rows, columns=['datetime', f'{renamed_city}_pm25'])
 
-        # Convert datetime column to datetime type
-        df['datetime'] = pd.to_datetime(df['datetime'])
+    # Convert datetime column to datetime type
+    df['datetime'] = pd.to_datetime(df['datetime'])
 
     # Setting the font size for labels
-        plt.rcParams.update({
-            'font.size': 14,
-            'axes.titlesize': 20,
-            'axes.labelsize': 18,
-            'xtick.labelsize': 14,
-            'ytick.labelsize': 14,
-            'legend.fontsize': 16
-        })
+    plt.rcParams.update({
+        'font.size': 30,
+        'axes.titlesize': 30,
+        'axes.labelsize': 28,
+        'xtick.labelsize': 16,
+        'ytick.labelsize': 16,
+        'legend.fontsize': 16
+    })
 
-        # Plotting with matplotlib
-        plt.figure(figsize=(12, 6))
-        plt.plot(df['datetime'], df[f'{global_city}_pm25'], label='PM2.5', linestyle='-', color='b', linewidth=0.75)
+    # Using ggplot style
+    plt.style.use('ggplot')
 
-        # Format the date to MM-YYYY
-        plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m-%Y'))
+    # Plotting with matplotlib
+    plt.figure(figsize=(14, 7))
+    plt.plot(df['datetime'], df[f'{renamed_city}_pm25'], label='PM2.5', linestyle='-', color='b', linewidth=1.5, alpha=0.7)
 
-        plt.legend()
-        plt.xlabel('Date')
-        plt.ylabel('Pollution Level μg/m3')
-        plt.title(f'PM2.5 Pollution Data for {global_city}')
-        plt.tight_layout()
+    # Format the date to MM-YYYY
+    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m-%Y'))
 
-        # Display the plot in Streamlit
-        return st.pyplot(plt)
+    plt.xticks(rotation=45)
 
+    plt.legend()
+    plt.xlabel('Date', fontsize = 18)
+    plt.ylabel('Pollution Level μg/m3', fontsize = 18)
+    plt.title(f'PM2.5 Pollution Data for {renamed_city}', fontsize = 28)
+    plt.grid(True, linestyle='--', alpha=0.6)
+    plt.tight_layout()
+
+    # Display the plot in Streamlit
+    return st.pyplot(plt)
+
+######################################################################################
+# UK ALL POLLUTANTS - ALL TIMEFRAMES
+
+def get_all_pollutants_uk(renamed_city, timeframe):
+    
+    conn = psql.connect(
+        dbname=dbname,
+        user=username,
+        password=password,
+        host=host,
+        port=port
+    )
+
+    cur = conn.cursor()
+
+    # SQL query to get the most recent datetime
+    latest_datetime_query = f"""
+        SELECT MAX(datetime)
+        FROM student.bw_air_pollution_data
+        WHERE {renamed_city}_pm25 IS NOT NULL
+    """
+    cur.execute(latest_datetime_query)
+    latest_datetime = cur.fetchone()[0]
+
+    # SQL query to select data from the most recent datetime to 7 days earlier
+    sql_query = f"""
+        SELECT datetime, {renamed_city}_pm25, {renamed_city}_o3, {renamed_city}_no2
+        FROM student.bw_air_pollution_data
+        WHERE {renamed_city}_pm25 IS NOT NULL
+        AND {renamed_city}_NO2 < 200
+        AND datetime BETWEEN %s AND %s
+        ORDER BY datetime
+    """
+
+    start_datetime = latest_datetime - timedelta(days= int(f'{timeframe}'))
+    cur.execute(sql_query, (start_datetime, latest_datetime))
+    rows = cur.fetchall()
+
+    # Close cursor and connection
+    cur.close()
+    conn.close()
+
+    # Convert query result to a pandas DataFrame
+    df = pd.DataFrame(rows, columns=['datetime', f'{renamed_city}_pm25', f'{renamed_city}_o3', f'{renamed_city}_no2'])
+
+    # Convert datetime column to datetime type
+    df['datetime'] = pd.to_datetime(df['datetime'])
+
+    # Setting the font size for labels
+    plt.rcParams.update({
+        'font.size': 30,
+        'axes.titlesize': 30,
+        'axes.labelsize': 28,
+        'xtick.labelsize': 16,
+        'ytick.labelsize': 16,
+        'legend.fontsize': 16
+    })
+
+    # Using ggplot style
+    plt.style.use('ggplot')
+
+    # Plotting with matplotlib
+    plt.figure(figsize=(14, 7))
+    plt.plot(df['datetime'], df[f'{renamed_city}_pm25'], label='PM2.5', linestyle='-', color='r', linewidth=1.5, alpha=0.7)
+    plt.plot(df['datetime'], df[f'{renamed_city}_o3'], label='O3', linestyle='-', color='g', linewidth=1.5, alpha=0.7)
+    plt.plot(df['datetime'], df[f'{renamed_city}_no2'], label='NO2', linestyle='-', color='b', linewidth=1.5, alpha=0.7)
+
+    # Format the date to MM-YYYY
+    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%d-%m-%Y %H:%M'))
+
+    plt.xticks(rotation=30)
+
+    plt.legend()
+    plt.xlabel('Date', fontsize=18)
+    plt.ylabel('Pollution Level μg/m3', fontsize=18)
+    plt.title(f'Pollution Data for {renamed_city}', fontsize=28)
+    plt.grid(True, linestyle='--', alpha=0.6)
+    plt.tight_layout()
+
+    # Display the plot in Streamlit
+    st.pyplot(plt)
+
+#################################################################################################
+# UK SINGLE POLLUTANTS - 1 MONTH
+
+def get_single_pollutant(renamed_city, timeframe, paramater_choice):
+    
+    conn = psql.connect(
+        dbname=dbname,
+        user=username,
+        password=password,
+        host=host,
+        port=port
+    )
+
+    cur = conn.cursor()
+
+    # SQL query to get the most recent datetime
+    latest_datetime_query = f"""
+        SELECT MAX(datetime)
+        FROM student.bw_air_pollution_data
+        WHERE {renamed_city}_{paramater_choice.lower()} IS NOT NULL
+
+    """
+    cur.execute(latest_datetime_query)
+    latest_datetime = cur.fetchone()[0]
+
+    # SQL query to select data from the most recent datetime to 7 days earlier
+    sql_query = f"""
+        SELECT datetime, {renamed_city}_{paramater_choice.lower()}
+        FROM student.bw_air_pollution_data
+        WHERE {renamed_city}_pm25 IS NOT NULL
+        AND datetime BETWEEN %s AND %s
+        AND {renamed_city}_{paramater_choice.lower()} != -999
+        ORDER BY datetime
+    """
+
+    start_datetime = latest_datetime - timedelta(days= int(f'{timeframe}'))
+    cur.execute(sql_query, (start_datetime, latest_datetime))
+    rows = cur.fetchall()
+
+    # Close cursor and connection
+    cur.close()
+    conn.close()
+
+    # Convert query result to a pandas DataFrame
+    df = pd.DataFrame(rows, columns=['datetime', f'{renamed_city}_{paramater_choice.lower()}'])
+
+    # Convert datetime column to datetime type
+    df['datetime'] = pd.to_datetime(df['datetime'])
+
+    # Setting the font size for labels
+    plt.rcParams.update({
+        'font.size': 30,
+        'axes.titlesize': 30,
+        'axes.labelsize': 28,
+        'xtick.labelsize': 16,
+        'ytick.labelsize': 16,
+        'legend.fontsize': 16
+    })
+
+    # Using ggplot style
+    plt.style.use('ggplot')
+
+    # Plotting with matplotlib
+    plt.figure(figsize=(14, 7))
+    plt.plot(df['datetime'], df[f'{renamed_city}_{paramater_choice.lower()}'], label= f'{paramater_choice.upper()}', linestyle='-', color='b', linewidth=1.5, alpha=0.7)
+
+    # Format the date to MM-YYYY
+    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%d-%m-%Y %H:%M'))
+
+    plt.xticks(rotation=30)
+
+    plt.legend()
+    plt.xlabel('Date', fontsize=18)
+    plt.ylabel('Pollution Level μg/m3', fontsize=18)
+    plt.title(f'{paramater_choice.upper()} Pollution Data for {renamed_city}', fontsize=28)
+    plt.grid(True, linestyle='--', alpha=0.6)
+    plt.tight_layout()
+
+    # Display the plot in Streamlit
+    st.pyplot(plt)
 
 
